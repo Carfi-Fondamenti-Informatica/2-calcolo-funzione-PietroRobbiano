3909c55bffb066b849a8b4fb2da4570f9d3495fb
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-04-03 17:38:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float x=0;
+    float y=0;
+    float a=0;
+    float b=0;
+    float z=0;
+    cin >> a;
+    cin >> b;
+    cin >> x;
+    cin >> y;
+    if (x<0 and y>0 ){
+        z=(a*x) - (b*y);
+        cout << z <<endl;
+    } else if ( x >= 0 and y<= 0 ) {
+        z=(a*x*x) - (b*y);
+        cout << z <<endl;
+    } else {
+        z=(a*x) + (b*y*y);
+        cout << z <<endl;
+    }
+
+    return 0;
 }
